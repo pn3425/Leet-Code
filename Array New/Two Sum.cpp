74234c09e3ed2dd75c82b99596c5bdf4f6ca02dd
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-06-24 00:10:11 +0530

    Create Two Sum.cpp
    Input: nums = [2,7,11,15], target = 9
    Output: [0,1]
    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
    
    https://leetcode.com/problems/two-sum/description/
    
    https://www.geeksforgeeks.org/problems/key-pair5616/1

---
+++ Array New/Two Sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+     vector<int>ans;
+     map<int,int>mpp;
+     for(int i=0;i<nums.size();i++){
+        int a = nums[i]; 
+        int more = target - a;
+        if(mpp.find(more) != mpp.end()){
+        
+          ans.push_back(mpp[more]);
+          ans.push_back(i);
+          return ans;
+        }
+       mpp[a]=i;
+     
+     }
+     return ans;
+    }
+};
