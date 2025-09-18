c9cc4ff3243f3af0010224836d0545ad57e1276e
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-09-18 22:13:51 +0530

    Create Pascal's Triangle.cpp
    Input: numRows = 5
    Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
    
    https://leetcode.com/problems/pascals-triangle/description/
    
    Also in Code Studio

---
+++ Striver Sheet/Array Hard/Pascal's Triangle.cpp
@@ -0,0 +1,26 @@
+//Problem 118
+
+class Solution {
+public:
+    vector<int> GenerateRow(int row){
+        vector<int> res;
+        long long ans = 1;
+        res.push_back(1);
+        for(int col=1; col<row; col++){
+             ans = ans * (row-col);
+             ans = ans / col;
+            res.push_back(ans);
+        }
+        return res;
+    }
+
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for(int i=1;i<=numRows;i++)
+        {
+            ans.push_back(GenerateRow(i));
+        }
+        return ans;
+        
+    }
+};
