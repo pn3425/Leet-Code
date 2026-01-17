1f3f86799b2142c48ca97611870650c58f3d7b0a
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2026-01-17 19:17:10 +0530

    Implemented normal approach
    Refactored binary search implementation in C++.

--- Striver Sheet/Binary Search/BS on 1D Array/Binary Search.cpp
+++ Striver Sheet/Binary Search/BS on 1D Array/Binary Search.cpp
@@ -13,3 +13,26 @@ int Bsearch(vector<int> &nums, int low, int high, int target)
        return Bsearch(nums, 0, nums.size()-1, target);  
     }
 };
+
+// Self from striver
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0, high = n-1;
+        while( low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(target > nums[mid]){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }  
+    return -1;
+    }
+};
