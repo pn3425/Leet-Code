276a1689487b1fabd726d64d3361d577656f9497
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2026-02-05 23:45:18 +0530

    Create check-if-one-string-is-rotation-of-another.cpp
    Added Brute Force Solution
    
    https://takeuforward.org/data-structure/check-if-one-string-is-rotation-of-another

---
+++ Striver Sheet/Strings/Basic & Easy String Problems/check-if-one-string-is-rotation-of-another.cpp
@@ -0,0 +1,22 @@
+/* Brute Force , TC -> O(n * n)
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length() != goal.length()) return false;
+        for(int i=0;i<s.length();i++){
+            string rotated = s.substr(i) + s.substr(0,i);
+            if(rotated == goal){
+                return true;
+            } 
+        }
+    return false;      
+    }
+}; */
+
+// Optimal 
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+     
+    }
+};
