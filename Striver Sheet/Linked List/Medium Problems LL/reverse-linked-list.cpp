9b945bb8056019938cf17dd5d9d49c760be29d5e
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2026-04-01 07:12:44 +0530

    Iterative approaches have been implemented
    https://leetcode.com/problems/reverse-linked-list/description/

---
+++ Striver Sheet/Linked List/Medium Problems LL/reverse-linked-list.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+//--------------------------- BRUTE FORCE : TC & SC -> O(2N) , O(N) (Using Stack) ---------------------------------
+
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* temp = head;
+//         stack<int> st;
+
+//         // Step 1: Access data of LL and push in the stack
+//         while(temp != NULL){
+//             st.push(temp -> val);
+//             temp = temp -> next;
+//         }
+
+//         temp = head; // Reset temp to head for iterating again
+
+//         while(temp != NULL){
+//             temp -> val = st.top();
+//             st.pop();
+//             temp = temp -> next;
+//          }
+//     return head;
+//     }
+// };
+ 
+//------------- OPTIMAL -> TC -> O(N), SC -> O(1) (NO EXTRA SPACE, NO STACK) (only changing links) ------------------
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* temp = head;
+        ListNode* prev = nullptr;
+
+        while(temp != NULL){
+            ListNode* front = temp -> next;
+            temp -> next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+};
