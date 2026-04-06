76c867d68b18d7cc72f770f58aa0a34dd40cd043
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2026-04-06 22:25:33 +0530

    Added optimal solution
    https://leetcode.com/problems/linked-list-cycle/

--- Striver Sheet/Linked List/Medium Problems LL/linked-list-cycle.cpp
+++ Striver Sheet/Linked List/Medium Problems LL/linked-list-cycle.cpp
@@ -8,6 +8,7 @@
  */
 
 //-------------------- BRUTE FORCE : Using hashmap, TC -> O(N * 2 * logN), SC -> O(N) ----------------------------
+/*
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
@@ -22,5 +23,21 @@ public:
             temp = temp -> next;
         }
         return false;
+    }
+}; */
+
+//---------------- Optimal : Tortoise & hare, TC -> O(N), SC -> O(1) Move by slow by 1 & fast by 2 ---------------
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast -> next != NULL){
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            if(slow == fast) return true;
+        }
+        return false; // Slow & fast never met
     }
 };
